b38fe786663b8f482ec187b375479301499d7685
Roilek <elior.papiernik@gmail.com>
2023-01-01 22:34:47 +0100

    Update description

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
 let projects = [
-    ['Project 1', 'Description 1', 'pictures/akula-left.png'],
-    ['Project 2', 'Description 2', 'pictures/akula-right.png'],
-    ['Project 3', 'Description 3', 'pictures/pompes-a-sirops.jpg']
+    ['Project 1', "En 2019 j'ai été traquenardé pour bricoler des toriis pour Japan Impact. C'était la première fois que je bricolais sérieusement et consciencieusement du bois. J'ai passé la quasi totalité de ma semaine à apprendre à utiliser la défonceuse, la perceuse à colonne et les ciseaux à bois, c'était super ! Comme dans l'art Japonais, il n'y a ni vis ni clou, tout tient par emboitement !", 'pictures/toriis.jpg'],
 ];
 
 for (let i = 0; i < 20; i++) {
-    j = i%projects.length;
+    let j = i%projects.length;
     const top_div = document.createElement("div");
+    top_div.className = "article"
 
-    const title = document.createElement("h1");
+    const title = document.createElement("h4");
     title.appendChild(document.createTextNode(projects[j][0]))
     top_div.appendChild(title)
 
