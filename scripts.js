ab1db75629afbee491c302a541628d7b4bb5914d
Roilek <elior.papiernik@gmail.com>
2023-01-01 18:13:19 +0100

    Add flexbox

--- scripts.js
+++ scripts.js
@@ -4,19 +4,20 @@ let projects = [
     ['Project 3', 'Description 3', 'pictures/pompes-a-sirops.jpg']
 ];
 
-for (let i = 0; i < projects.length; i++) {
+for (let i = 0; i < 20; i++) {
+    j = i%projects.length;
     const top_div = document.createElement("div");
 
     const title = document.createElement("h1");
-    title.appendChild(document.createTextNode(projects[i][0]))
+    title.appendChild(document.createTextNode(projects[j][0]))
     top_div.appendChild(title)
 
     const description = document.createElement('p')
-    description.appendChild(document.createTextNode(projects[i][1]))
+    description.appendChild(document.createTextNode(projects[j][1]))
     top_div.appendChild(description)
 
     const image = document.createElement('img')
-    image.src = projects[i][2]
+    image.src = projects[j][2]
     top_div.appendChild(image)
 
     document.getElementById("projects").appendChild(top_div);
